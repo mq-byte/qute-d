b7d59d0350bb016f716f71f27edf706b1d987357
邱阳 <qiuyang02@qingting.fm>
2020-08-26 00:45:10 +0800

    split

---
+++ src/AutoScroll/index.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import styles from './styles.module.css'
+
+const AutoScroll = ({ speed }) => {
+  return <div className={styles.test}>
+    {speed}
+  </div>
+}
+export default AutoScroll

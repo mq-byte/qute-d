7e483c6e87b1e989f221307fb9a96d0273242fce
yang-qiu <yang.qiu@binance.com>
2020-08-26 15:21:03 +0800

    Updates

---
+++ precache-manifest.261a0e22e37d2bdfb19cef0698a85d9b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6a39bd90b1d75e2f7b48a276b8d13e11",
+    "url": "./index.html"
+  },
+  {
+    "revision": "c5809efbf5d57ff212b0",
+    "url": "./static/css/main.63b2a50c.chunk.css"
+  },
+  {
+    "revision": "29146daf6357363140bf",
+    "url": "./static/js/2.40ab5634.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.40ab5634.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c5809efbf5d57ff212b0",
+    "url": "./static/js/main.028e8789.chunk.js"
+  },
+  {
+    "revision": "c6ef8bbb854c212284cf",
+    "url": "./static/js/runtime-main.13545fa8.js"
+  }
+]);
